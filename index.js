d68ae7f147f1ec80ac497e2277e32eafdafd1d98
fleish80 <Fleish.D@gmail.com>
2018-09-28 22:29:52 +0300

    Lesson 5

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', (req, res) => {
+    console.log('GET request');
+    res.send({name: 'yoshi'});
+});
+
 //listen for requests
 app.listen(process.env.port || 4000, () => {
     console.log('now listening for requests')
-})
+});
